a6ef091f152d42a77057d0f88ad6f2a3225563ef
mathieu amacher <amacher_mathieu@hotmail.com>
2025-01-27 22:29:42 +0100

    feature eng

--- src2/preprocess_sunshine.py
+++ src2/preprocess_sunshine.py
@@ -46,4 +46,21 @@ class sunshine_transformer(BaseEstimator, TransformerMixin):
                 if pd.isna(row[self.col_select]) else row[self.col_select],
                 axis=1)
 
+        # X[self.col_select +'_mean_7'] = X[self.col_select].rolling(window=7, min_periods=1).mean()
+        # X[self.col_select + '_std_7'] = X[self.col_select].rolling(window=7, min_periods=1).std()
+        # X[self.col_select + '_max_7'] = X[self.col_select].rolling(window=7, min_periods=1).max()
+        # X[self.col_select + '_min_7'] = X[self.col_select].rolling(window=7, min_periods=1).min()
+
+        # X[self.col_select +'_mean_5'] = X[self.col_select].rolling(window=5, min_periods=1).mean()
+        # X[self.col_select + '_std_5'] = X[self.col_select].rolling(window=5, min_periods=1).std()
+        # X[self.col_select + '_max_5'] = X[self.col_select].rolling(window=5, min_periods=1).max()
+        # X[self.col_select + '_min_5'] = X[self.col_select].rolling(window=5, min_periods=1).min()
+
+        # X[self.col_select +'_mean_3'] = X[self.col_select].rolling(window=3, min_periods=1).mean()
+        # X[self.col_select + '_std_3'] = X[self.col_select].rolling(window=3, min_periods=1).std()
+        # X[self.col_select + '_max_3'] = X[self.col_select].rolling(window=3, min_periods=1).max()
+        # X[self.col_select + '_min_3'] = X[self.col_select].rolling(window=3, min_periods=1).min()
+
+        X[self.col_select + '_diff'] = X[self.col_select].diff()
+
         return X
