d6f18c7d5f029287399228f617c2c4d61d9eee95
mathieu amacher <amacher_mathieu@hotmail.com>
2025-01-27 22:30:12 +0100

    feature eng

--- src2/preprocess_temp.py
+++ src2/preprocess_temp.py
@@ -4,7 +4,7 @@ class preprocess_temp():
 
     def __init__(self, col_select : str = 'MinTemp', method : str = 'linear'):
 
-        self.col = col_select
+        self.col_select = col_select
         self.method = method
 
     def fit(self, X, y = None):
@@ -12,6 +12,24 @@ class preprocess_temp():
 
     def transform(self, X):
 
-        X[self.col] = X[self.col].interpolate(method = self.method)
+        X[self.col_select] = X[self.col_select].interpolate(method = self.method)
+
+        # X[self.col_select +'_mean_7'] = X[self.col_select].rolling(window=7, min_periods=1).mean()
+        # X[self.col_select + '_std_7'] = X[self.col_select].rolling(window=7, min_periods=1).std()
+        # X[self.col_select + '_max_7'] = X[self.col_select].rolling(window=7, min_periods=1).max()
+        # X[self.col_select + '_min_7'] = X[self.col_select].rolling(window=7, min_periods=1).min()
+
+        # X[self.col_select +'_mean_5'] = X[self.col_select].rolling(window=5, min_periods=1).mean()
+        # X[self.col_select + '_std_5'] = X[self.col_select].rolling(window=5, min_periods=1).std()
+        # X[self.col_select + '_max_5'] = X[self.col_select].rolling(window=5, min_periods=1).max()
+        # X[self.col_select + '_min_5'] = X[self.col_select].rolling(window=5, min_periods=1).min()
+
+        # X[self.col_select +'_mean_3'] = X[self.col_select].rolling(window=3, min_periods=1).mean()
+        # X[self.col_select + '_std_3'] = X[self.col_select].rolling(window=3, min_periods=1).std()
+        # X[self.col_select + '_max_3'] = X[self.col_select].rolling(window=3, min_periods=1).max()
+        # X[self.col_select + '_min_3'] = X[self.col_select].rolling(window=3, min_periods=1).min()
+
+        X[self.col_select + '_diff'] = X[self.col_select].diff()
+
 
         return X
