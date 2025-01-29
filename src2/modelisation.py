dec2de9c19c3c0e344a86a976b7475d07926942a
mathieu amacher <amacher_mathieu@hotmail.com>
2025-01-29 21:12:34 +0100

    new model

--- src2/modelisation.py
+++ src2/modelisation.py
@@ -5,7 +5,7 @@ import numpy as np
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import confusion_matrix, classification_report, f1_score, roc_auc_score, precision_recall_curve, brier_score_loss
 from sklearn.preprocessing import MinMaxScaler
-from sklearn.ensemble import RandomForestClassifier
+from sklearn.linear_model import LogisticRegression
 
 
 def modelisation(df):
@@ -22,24 +22,16 @@ def modelisation(df):
     poids = y_train.value_counts(normalize = True)
 
 
-    model = RandomForestClassifier(
-                n_estimators = 755,
-                max_depth = 8,
-                max_features = 'sqrt',
-                min_samples_split = 3,
-                min_samples_leaf = 15,
-                criterion = 'gini',
-                bootstrap = True,
-                class_weight={0: 1/poids[0], 1: 1/poids[1]},
-                random_state=42,
-            )
+    model = LogisticRegression(verbose = 0,
+                               C = 1,
+                               max_iter = 1000,
+                               penalty = 'l1',
+                               solver = 'liblinear',
+                               intercept_scaling = 0.1,
+                               l1_ratio = 0.5,
+                               tol = 0.01)
 
     model.fit(X_train_scaled, y_train)
-
-    print(y_train.value_counts(normalize = True))
-    print(y_train.value_counts())
-
-    prob = model.predict_proba(X_test_scaled)
 
     y_pred_prob1 = model.predict_proba(X_test_scaled)[:, 1]
 
@@ -51,8 +43,7 @@ def modelisation(df):
         precision, recall, thresholds = precision_recall_curve(y_test, y_pred_prob1)
         diff = abs(precision - recall)
         threshold = thresholds[diff.argmin()]
-    print(threshold)
-
+    # print(threshold)
 
     y_pred = (y_pred_prob1 > threshold).astype(int)
 
