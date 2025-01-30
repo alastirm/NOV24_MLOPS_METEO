32634f55df5f987c6b68a7a22639dca23e48d55d
mathieu amacher <amacher_mathieu@hotmail.com>
2025-01-30 15:25:59 +0100

    preproc

--- src2/preprocess.py
+++ src2/preprocess.py
@@ -11,6 +11,9 @@ from preprocess_temp import preprocess_temp
 from preprocess_rainfall_raintoday import rainfall_raintoday_transformer
 from preprocess_wind import wind_speed_transformer, wind_dir_transformer
 from preprocess_humidity_pressure import VoisinageNAImputer
+from preprocess_evaporation import evaporation_transformer
+from preprocess_cloud import cloud_transformer
+
 
 from modelisation import modelisation
 
@@ -42,26 +45,41 @@ def preprocessing(url_data : str = '../data/weatherAUS.csv', city : str = 'Sydne
 
     transformers = []
 
-    if 'Sunshine' not in columns_to_drop:
-        transformers.append(('sunshine_transformer', sunshine_transformer()))
     if 'RainTomorrow' not in columns_to_drop:
         transformers.append(('raintomorrow_transformer', raintomorrow_transformer()))
+
+    if 'Evaporation' not in columns_to_drop:
+        transformers.append(('evaporation_transformer', evaporation_transformer()))
+
+    if 'Sunshine' not in columns_to_drop:
+        transformers.append(('sunshine_transformer', sunshine_transformer()))
+
     if 'Date' not in columns_to_drop:
         transformers.append(('date_transformer', preprocess_date_transformer()))
+
     for temp_col in ['MinTemp', 'MaxTemp', 'Temp9am', 'Temp3pm']:
         if temp_col not in columns_to_drop:
             transformers.append((f'{temp_col.lower()}_transformer', preprocess_temp(col_select=temp_col)))
+
+    for cloud_col in ['Cloud9am', 'Cloud3pm']:
+        if cloud_col not in columns_to_drop:
+            transformers.append((f'{cloud_col.lower()}_transformer', cloud_transformer(col_select=cloud_col)))
+
     for wind_col in ['WindGustSpeed', 'WindSpeed9am', 'WindSpeed3pm']:
         if wind_col not in columns_to_drop:
             transformers.append((f'{wind_col.lower()}_transformer', wind_speed_transformer(col_select=wind_col)))
+
     for wind_dir_col in ['WindGustDir', 'WindDir9am', 'WindDir3pm']:
         if wind_dir_col not in columns_to_drop:
             transformers.append((f'{wind_dir_col.lower()}_transformer', wind_dir_transformer(col_select=wind_dir_col)))
+
     if 'Rainfall' not in columns_to_drop:
         transformers.append(('rainfall_transformer', rainfall_raintoday_transformer(city=city)))
+
     for humidity_col in ['Humidity9am', 'Humidity3pm']:
         if humidity_col not in columns_to_drop:
             transformers.append((f'{humidity_col.lower()}_imputer', VoisinageNAImputer(column=humidity_col)))
+
     for pressure_col in ['Pressure9am', 'Pressure3pm']:
         if pressure_col not in columns_to_drop:
             transformers.append((f'{pressure_col.lower()}_imputer', VoisinageNAImputer(column=pressure_col)))
