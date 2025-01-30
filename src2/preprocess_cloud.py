270f3511108669c084f1d4acb2de8d62575002c0
mathieu amacher <amacher_mathieu@hotmail.com>
2025-01-30 15:27:09 +0100

    all

--- src2/preprocess_cloud.py
+++ src2/preprocess_cloud.py
@@ -1,18 +1,30 @@
-import pandas
+import pandas as pd
+import numpy as np
 
-class preprocess_temp():
+from sklearn.base import BaseEstimator, TransformerMixin
 
-    def __init__(self, col_select : str = 'MinTemp', method : str = 'linear'):
 
+class cloud_transformer(BaseEstimator, TransformerMixin):
+    def __init__(self, col_select: str = 'Cloud9am', col_target: str = 'RainTomorrow', method: str = 'median'):
+
+        self.dict_cld = {}
         self.col_select = col_select
+        self.col_target = col_target
         self.method = method
 
-    def fit(self, X, y = None):
+
+    def fit(self, X, y=None):
+
+        self.dict_cld = X.groupby(self.col_target)[self.col_select].agg(self.method).to_dict()
+
         return self
+
 
     def transform(self, X):
 
-        X[self.col_select] = X[self.col_select].interpolate(method = self.method)
+        X[self.col_select] = X.apply(
+            lambda row: self.dict_cld.get(row[self.col_target], row[self.col_select])
+            if pd.isna(row[self.col_select]) else row[self.col_select],axis=1)
 
         X[self.col_select +'_mean_7'] = X[self.col_select].rolling(window=7, min_periods=1).mean()
         X[self.col_select + '_std_7'] = X[self.col_select].rolling(window=7, min_periods=1).std()
@@ -30,6 +42,5 @@ class preprocess_temp():
         X[self.col_select + '_min_3'] = X[self.col_select].rolling(window=3, min_periods=1).min()
 
         X[self.col_select + '_diff'] = X[self.col_select].diff()
-
 
         return X
