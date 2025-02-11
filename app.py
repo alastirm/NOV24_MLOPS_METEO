5ea50f13a378915aaf16e05537fb5b5dca066ee6
mathieu amacher <amacher_mathieu@hotmail.com>
2025-02-11 19:09:33 +0100

    r

--- app.py
+++ app.py
@@ -4,12 +4,5 @@ import streamlit as st
 with st.sidebar:
     st.title('MeteoStralia')
 
-    if st.button("Home"):
-        st.switch_page("app.py")
-    if st.button("By Location"):
-        st.switch_page("pages/page_location.py")
-    # if st.button("Page 2"):
-    #     st.switch_page("pages/page_2.py")
-
 
 st.title('MeteoStralia')
