de9c0c248e045155c529285e9fea4f6649a86fb7
mathieu amacher <amacher_mathieu@hotmail.com>
2025-02-11 15:57:53 +0100

    amélioration

--- src2/preprocess_raintomorrow.py
+++ src2/preprocess_raintomorrow.py
@@ -13,10 +13,27 @@ class raintomorrow_transformer(BaseEstimator, TransformerMixin):
 
     def transform(self, X):
 
-        X['lag_raintomorrow'] = X['RainTomorrow'].shift()
-        X['lag_raintomorrow'] = X['lag_raintomorrow'].replace(['Yes', 'No'], [1, 0])
+        X['RainTomorrow'] = X['RainTomorrow'].replace(['Yes', 'No'], [1, 0]).astype(float)
+        X.dropna(subset = 'RainTomorrow', inplace = True)
 
-        X['RainTomorrow'] = X['RainTomorrow'].replace(['Yes', 'No'], [1, 0])
+        X['lag_raintomorrow'] = X['RainTomorrow'].shift()
+
+        X['RainTomorrow' +'_mean_7'] = X['RainTomorrow'].rolling(window=7, min_periods=1).mean().dropna()
+        X['RainTomorrow' + '_std_7'] = X['RainTomorrow'].rolling(window=7, min_periods=1).std().dropna()
+        X['RainTomorrow' + '_max_7'] = X['RainTomorrow'].rolling(window=7, min_periods=1).max().dropna()
+        X['RainTomorrow' + '_min_7'] = X['RainTomorrow'].rolling(window=7, min_periods=1).min().dropna()
+
+        X['RainTomorrow' +'_mean_5'] = X['RainTomorrow'].rolling(window=5, min_periods=1).mean().dropna()
+        X['RainTomorrow' + '_std_5'] = X['RainTomorrow'].rolling(window=5, min_periods=1).std().dropna()
+        X['RainTomorrow' + '_max_5'] = X['RainTomorrow'].rolling(window=5, min_periods=1).max().dropna()
+        X['RainTomorrow' + '_min_5'] = X['RainTomorrow'].rolling(window=5, min_periods=1).min().dropna()
+
+        X['RainTomorrow' +'_mean_3'] = X['RainTomorrow'].rolling(window=3, min_periods=1).mean().dropna()
+        X['RainTomorrow' + '_std_3'] = X['RainTomorrow'].rolling(window=3, min_periods=1).std().dropna()
+        X['RainTomorrow' + '_max_3'] = X['RainTomorrow'].rolling(window=3, min_periods=1).max().dropna()
+        X['RainTomorrow' + '_min_3'] = X['RainTomorrow'].rolling(window=3, min_periods=1).min().dropna()
+
+
         X.dropna(subset = 'RainTomorrow', inplace = True)
 
         return X
