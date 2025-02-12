b3fd493908dea85e09f36ed07162ffb2948fbfc2
mathieu amacher <amacher_mathieu@hotmail.com>
2025-02-12 23:23:09 +0100

    retirer rolling

--- src2/preprocess_raintomorrow.py
+++ src2/preprocess_raintomorrow.py
@@ -16,24 +16,5 @@ class raintomorrow_transformer(BaseEstimator, TransformerMixin):
         X['RainTomorrow'] = X['RainTomorrow'].replace(['Yes', 'No'], [1, 0]).astype(float)
         X.dropna(subset = 'RainTomorrow', inplace = True)
 
-        X['lag_raintomorrow'] = X['RainTomorrow'].shift()
-
-        X['RainTomorrow' +'_mean_7'] = X['RainTomorrow'].rolling(window=7, min_periods=1).mean().dropna()
-        X['RainTomorrow' + '_std_7'] = X['RainTomorrow'].rolling(window=7, min_periods=1).std().dropna()
-        X['RainTomorrow' + '_max_7'] = X['RainTomorrow'].rolling(window=7, min_periods=1).max().dropna()
-        X['RainTomorrow' + '_min_7'] = X['RainTomorrow'].rolling(window=7, min_periods=1).min().dropna()
-
-        X['RainTomorrow' +'_mean_5'] = X['RainTomorrow'].rolling(window=5, min_periods=1).mean().dropna()
-        X['RainTomorrow' + '_std_5'] = X['RainTomorrow'].rolling(window=5, min_periods=1).std().dropna()
-        X['RainTomorrow' + '_max_5'] = X['RainTomorrow'].rolling(window=5, min_periods=1).max().dropna()
-        X['RainTomorrow' + '_min_5'] = X['RainTomorrow'].rolling(window=5, min_periods=1).min().dropna()
-
-        X['RainTomorrow' +'_mean_3'] = X['RainTomorrow'].rolling(window=3, min_periods=1).mean().dropna()
-        X['RainTomorrow' + '_std_3'] = X['RainTomorrow'].rolling(window=3, min_periods=1).std().dropna()
-        X['RainTomorrow' + '_max_3'] = X['RainTomorrow'].rolling(window=3, min_periods=1).max().dropna()
-        X['RainTomorrow' + '_min_3'] = X['RainTomorrow'].rolling(window=3, min_periods=1).min().dropna()
-
-
-        X.dropna(subset = 'RainTomorrow', inplace = True)
 
         return X
